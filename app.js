c1cb86ac6dfd5078a574c5b456820272ef4b47f1
Jose Moreno <morenojr90@yahoo.com>
2018-06-21 10:53:32 -0700

    some stuff

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
-
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser')
+
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended:false }));
+app.use(cookieParser)
 app.set('view engine', 'pug');
 
 app.get('/', (req,res,next) => {
@@ -15,7 +17,7 @@ app.get('/cards', (req,res) => {
 })
 
 app.get('/hello', (req,res) => {
-  res.render('hello');
+  res.render('hello', { name: req.cookies.username});
 })
 
 app.post('/hello', (req,res) => {
