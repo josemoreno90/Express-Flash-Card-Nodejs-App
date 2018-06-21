e421ad6677d78c45b3e137541aab4a671a03c895
Jose Moreno <morenojr90@yahoo.com>
2018-06-21 13:08:53 -0700

    add redirect to post route, add cookie of user, add if logic that checks if current user exist

--- app.js
+++ app.js
@@ -4,25 +4,38 @@ const cookieParser = require('cookie-parser')
 
 
 const app = express();
+
+
 app.use(bodyParser.urlencoded({ extended:false }));
-app.use(cookieParser)
+app.use(cookieParser())
 app.set('view engine', 'pug');
 
-app.get('/', (req,res,next) => {
-  res.render('index');
+app.get('/', (req,res) => {
+  const name = req.cookies.username;
+  if (name) {
+    res.render('index', { name });
+  } else {
+    res.redirect('/hello');
+  }
 })
 
 app.get('/cards', (req,res) => {
-  res.render('card', {prompt: "Who is buried in Grant's Tomb?", colors});
+  const hint = "He bears his name"
+  res.render('card', {prompt: "Who is buried in Grant's Tomb?", hint});
 })
 
 app.get('/hello', (req,res) => {
-  res.render('hello', { name: req.cookies.username});
+  const name = req.cookies.username;
+  if (name) {
+    res.render('/');
+  } else {
+    res.render('hello');
+  }
 })
 
 app.post('/hello', (req,res) => {
   res.cookie('username', req.body.username)
-  res.render('hello', { name: req.body.username });
+  res.redirect('/');
 })
 
 app.listen(3000);
